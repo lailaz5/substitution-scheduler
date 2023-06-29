e5244e5afb6fd9529f6e29abcef731bdf27724d8
Laila <124719945+lailaz5@users.noreply.github.com>
2023-06-29 19:16:58 +0200

    Timetable fetching
    Added WIP code to fetch a teacher's timetable

--- api.py
+++ api.py
@@ -1,9 +1,10 @@
+
+    # WIP
+
 from bs4 import BeautifulSoup
 import requests
 
 html_text = requests.get('https://www.isarchimede.edu.it/Orario/index.html').text
-
-
 table = BeautifulSoup(html_text, 'lxml').find('table')
 
 teachers_links = []
@@ -13,7 +14,18 @@ for row in table.find_all('tr'):
         if 'DOCENTI' in cell.text.strip():
             links = cell.find_all('a')
             for link in links:
-                #print(link.text.strip())
                 teachers_links.append(link['href'])
 
-#print(teachers_links)
+#teacher = 'Piccolo Gianluca.html' ---> n.112 in the list
+
+teacher_html = requests.get(f'https://www.isarchimede.edu.it/Orario/{teachers_links[112]}').text
+teacher_table = BeautifulSoup(teacher_html, 'lxml').find('table')
+
+timetable = []
+
+for row in teacher_table.find_all('tr'):
+    timetable.append(row.text.strip())
+    '''for cell in row.find_all('td'):
+        classes.append(cell.text.strip())'''
+
+print(timetable)
