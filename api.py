946e22970f6ceda91f91ed3e71e66dcb5c560a72
Laila <124719945+lailaz5@users.noreply.github.com>
2023-07-02 19:48:21 +0200

    Small changes
    Addition of a dictionary for storing teacher names and links.

--- api.py
+++ api.py
@@ -5,27 +5,26 @@ from bs4 import BeautifulSoup
 import requests
 
 html_text = requests.get('https://www.isarchimede.edu.it/Orario/index.html').text
-table = BeautifulSoup(html_text, 'lxml').find('table')
+main_table = BeautifulSoup(html_text, 'lxml').find('table')
 
-teachers_links = []
+teachers_dict = {}
 
-for row in table.find_all('tr'):
+for row in main_table.find_all('tr'):
     for cell in row.find_all('td'):
         if 'DOCENTI' in cell.text.strip():
             links = cell.find_all('a')
             for link in links:
-                teachers_links.append(link['href'])
+                teacher_name = link.text.strip()
+                teacher_link = link['href']
+                teachers_dict[teacher_name] = teacher_link
 
-#teacher = 'Piccolo Gianluca.html' ---> n.112 in the list
-
-teacher_html = requests.get(f'https://www.isarchimede.edu.it/Orario/{teachers_links[112]}').text
+teacher_html = requests.get(f'https://www.isarchimede.edu.it/Orario/{teachers_dict["Piccolo Gianluca"]}').text
 teacher_table = BeautifulSoup(teacher_html, 'lxml').find('table')
 
 timetable = []
 
 for row in teacher_table.find_all('tr'):
-    timetable.append(row.text.strip())
-    '''for cell in row.find_all('td'):
-        classes.append(cell.text.strip())'''
-
-print(timetable)
+    for cell in row.find_all('td'):
+        timetable.append(cell.text.strip())
+    
+#print(timetable)
