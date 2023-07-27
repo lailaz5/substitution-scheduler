3772aa9e86ad086e78eb5110ca70685d9f1835c4
Laila <124719945+lailaz5@users.noreply.github.com>
2023-07-27 19:08:45 +0200

    WIP of the website
    Navbar component added.

--- frontend/substitution-app/src/App.jsx
+++ frontend/substitution-app/src/App.jsx
@@ -1,11 +1,17 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
+import Home from './pages/Home';
 import './styles/App.css';
 
 const App = () => {
   return (
-    <>
-      <Navbar />
-    </>
+    <Router>
+      <Navbar/>
+      <Routes>
+        <Route exact path="/" element={<Home/>} />
+      </Routes>
+    </Router>
   );
 }
 
