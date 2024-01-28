b35cbd8171a5e65de3397fc60fc7cc96075bf9f6
Laila <124719945+lailaz5@users.noreply.github.com>
2024-01-28 23:03:40 +0100

    Small changes

--- backend/api.py
+++ backend/api.py
@@ -9,24 +9,12 @@ app = Flask(__name__)
 CORS(app)
 
 
-@app.route('/<path:teacher_name>', methods=['GET'])
-def timetable(teacher_name):
-    decoded_teacher_name = unquote(teacher_name)  
-    try:
-        timetable = fetch_timetable(decoded_teacher_name)
-        response_JSON = json.dumps(timetable, ensure_ascii=False, indent=2)
-        response = Response(response_JSON, content_type='application/json; charset=utf-8')
-        
-        return response, 200
-    except KeyError:
-        return jsonify({'error': 'Teacher not found'}), 404
-
-
 @app.route('/teachers', methods=['GET'])
 def teachers_list():
     teachers_dict = fetch_teachers()
     teachers_list = list(teachers_dict.keys())
     return jsonify(teachers_list), 200
+
 
 @app.route('/<path:teacher_name>_classes', methods=['GET'])
 def classes(teacher_name):
@@ -39,6 +27,20 @@ def classes(teacher_name):
         return response, 200
     except KeyError:
         return jsonify({'error': 'No results found.'}), 404
+
+
+@app.route('/<path:teacher_name>', methods=['GET'])
+def timetable(teacher_name):
+    decoded_teacher_name = unquote(teacher_name)  
+    try:
+        timetable = fetch_timetable(decoded_teacher_name)
+        response_JSON = json.dumps(timetable, ensure_ascii=False, indent=2)
+        response = Response(response_JSON, content_type='application/json; charset=utf-8')
+        
+        return response, 200
+    except KeyError:
+        return jsonify({'error': 'Teacher not found'}), 404
+
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
